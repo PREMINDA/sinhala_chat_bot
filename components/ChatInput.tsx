33025e7a436ef23e07e013ab7b388815cf26de50
PREMINDA <premindabandara0777@gmail.com>
2023-04-08 13:42:20 +0530

    chat input style change

--- components/ChatInput.tsx
+++ components/ChatInput.tsx
@@ -46,9 +46,10 @@ const ChatInput = ({ onSend, disabled }: InputProps) => {
             </div>
          )}{!disabled && (
             <button
+            className="flex justify-center items-center"
                onClick={() => sendInput()}
             >
-               <svg xmlns="http://www.w3.org/2000/svg" className="bi bi-send w-6 h-6 mr-2 opacity-50" viewBox="0 0 16 16">
+               <svg xmlns="http://www.w3.org/2000/svg" className="bi bi-send w-6 h-6 mr-2 opacity-50 " viewBox="0 0 16 16">
                   <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 
                   0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 
                   0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 
--- components/SpeechToText.jsx
+++ components/SpeechToText.jsx
@@ -33,16 +33,14 @@ const SpeechToText = () => {
   }
 
   return (
-    <div className="inline-block">
+    <div className="flex justify-center items-center">
       <button
         onClick={handleStart}
         className={isAnimating ? 'animate-pulse' : ''}
         onAnimationEnd={handleAnimationEnd}
       >
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
-          <path d="M9 12a3 3 0 116 0v3a3 3 0 01-6 0v-3zm-3 0a6 6 0 1112 0v3a6 6 0 01-12 0v-3z" />
-          <path fillRule="evenodd" d="M10 2a8 8 0 018 8v3a1 1 0 11-2 0v-3a6 6 0 10-12 0v3a1 1 0 11-2 0v-3a8 8 0 018-8zm-6 8a1 1 0 011-1h10a1 1 0 110 2H5a1 1 0 01-1-1z" clipRule="evenodd" />
-        </svg>
+          <svg className="bi bi-send w-8 h-8 mr-2 opacity-40 bg-slate-300 p-1 rounded-md" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 2c1.103 0 2 .897 2 2v7c0 1.103-.897 2-2 2s-2-.897-2-2v-7c0-1.103.897-2 2-2zm0-2c-2.209 0-4 1.791-4 4v7c0 2.209 1.791 4 4 4s4-1.791 4-4v-7c0-2.209-1.791-4-4-4zm8 9v2c0 4.418-3.582 8-8 8s-8-3.582-8-8v-2h2v2c0 3.309 2.691 6 6 6s6-2.691 6-6v-2h2zm-7 13v-2h-2v2h-4v2h10v-2h-4z"/>
+          </svg>
       </button>
     </div>
   );
