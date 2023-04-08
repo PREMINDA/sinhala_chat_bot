3f38bd61d8e1a222728c56372a75b33b8cad8496
PREMINDA <premindabandara0777@gmail.com>
2023-04-08 13:18:20 +0530

    alert

--- components/Alert.tsx
+++ components/Alert.tsx
@@ -3,21 +3,17 @@ import React from 'react'
 
 export default function Alert({message}:AlertMessage) {
   return (
-    <div id="alert-additional-content-1" className="p-4 mb-4 text-blue-800 border border-blue-300 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400 dark:border-blue-800" role="alert">
+    <div className="p-4 m-0 border w-1/5 h-36 fixed inset-0 bg-opacity-30 backdrop-blur-sm flex  items-center justify-center border-gray-300 rounded-lg bg-gray-50 dark:border-gray-600 dark:bg-thembg" role="alert">
         <div className="flex items-center">
-            <svg aria-hidden="true" className="w-5 h-5 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path></svg>
-            <span className="sr-only">Info</span>
-            <h3 className="text-lg font-medium">Something went wrong try again</h3>
+            <svg aria-hidden="true" className="w-5 h-5 mr-2 text-gray-800 dark:text-gray-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"></path></svg>
+            <span className="sr-only">Something went Wrong try again</span>
+            <h3 className="text-lg font-medium text-gray-800 dark:text-gray-300">This is a dark alert</h3>
         </div>
-        <div className="mt-2 mb-4 text-sm">
-            More info about this info alert goes here. This example text is going to run a bit longer so that you can see how spacing within an alert works with this kind of content.
+        <div className="mt-2 mb-4 text-sm text-gray-800 dark:text-gray-300">
+            {message}
         </div>
         <div className="flex">
-            <button type="button" className="text-white bg-blue-800 hover:bg-blue-900 focus:ring-4 focus:outline-none focus:ring-blue-200 font-medium rounded-lg text-xs px-3 py-1.5 mr-2 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-            <svg aria-hidden="true" className="-ml-0.5 mr-2 h-4 w-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10 12a2 2 0 100-4 2 2 0 000 4z"></path><path fill-rule="evenodd" d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z" clip-rule="evenodd"></path></svg>
-            View more
-            </button>
-            <button type="button" className="text-blue-800 bg-transparent border border-blue-800 hover:bg-blue-900 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-200 font-medium rounded-lg text-xs px-3 py-1.5 text-center dark:hover:bg-blue-600 dark:border-blue-600 dark:text-blue-400 dark:hover:text-white dark:focus:ring-blue-800" data-dismiss-target="#alert-additional-content-1" aria-label="Close">
+            <button type="button" className="text-gray-800 bg-transparent border border-gray-700 hover:bg-gray-800 hover:text-white focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-xs px-3 py-1.5 text-center dark:border-gray-600 dark:hover:bg-gray-600 dark:focus:ring-gray-800 dark:text-gray-300 dark:hover:text-white" data-dismiss-target="#alert-additional-content-5" aria-label="Close">
             Dismiss
             </button>
         </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import useState from 'react-usestateref'
 import ChatMessage from '@/components/ChatMessage'
 import ChatInput from '@/components/ChatInput'
 import styles from '@/styles/Home.module.css'
+import Alert from '@/components/Alert'
 
 
 const inter = Inter({ subsets: ['latin'] })
@@ -41,18 +42,21 @@ export default function Home() {
   }
 
   return (
-    <main className='bg-thembg'>
-    <div className='relative max-w-4xl mx-auto h-screen flex flex-col justify-between'>
-      <div className='px-4 pt-10 '>
-        {message && message.map((item) => { return <ChatMessage key={item.key} from={item.from} user_input={item.user_input} text={item.text}/> })}
-        {message.length==0&&<p className='text-center text-gray-400'>I am at your Service</p>}
-      </div>
-      <div className='sticky bottom-0 w-full px-4 z-12'>
-        <div className={styles.inputBg}>
-         <ChatInput onSend={(input)=>callApi(input)} disabled={loading}/>
+    <>
+      <main className='bg-thembg'>
+        <div className='relative max-w-4xl mx-auto h-screen flex flex-col justify-between'>
+          <div className='px-4 pt-10 '>
+            {message && message.map((item) => { return <ChatMessage key={item.key} from={item.from} user_input={item.user_input} text={item.text}/> })}
+            {message.length==0&&<p className='text-center text-gray-400'>I am at your Service</p>}
+          </div>
+          <div className='sticky bottom-0 w-full px-4 z-12'>
+            <div className={styles.inputBg}>
+            <ChatInput onSend={(input)=>callApi(input)} disabled={loading}/>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
-    </main>
+      </main>
+      
+    </>
   )
 }
